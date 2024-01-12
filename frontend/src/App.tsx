0095bdb0c08484bfbbb56bf838e5928f9456e06d
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-12 11:09:42 -0300

    Configurando o elsint tailwindcss e o shadcnui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import 'tailwindcss/tailwind.css'
+import './global.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
   return (
-    <div>
-      Hello World
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
     </div>
   )
 }
-
-export default App
