75218ddacd3d2bfd28b65756121649cd49475737
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-12 12:34:35 -0300

    Criando a pagina de login

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,13 +1,24 @@
 /* eslint-disable prettier/prettier */
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
     return (
-        <div>
-            <h1>Autenticacão</h1>
-            <div>
+        <div className="min-h-screen grid grid-cols-2">
+            <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+                <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+                    <Pizza className="w-5 h-5" />
+                    <span className="font-semibold">Pizza Shop</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do parceiro &copy @pizza.shop - {new Date().getFullYear()}
+                </footer>
+
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
                 <Outlet />
             </div>
-        </div>
+        </div >
     )
 }
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,11 +1,37 @@
 /* eslint-disable prettier/prettier */
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
     return (
         <>
             <Helmet title="login" />
-            <h1>SignIn</h1>
+            <div className="p-8">
+                <div className="w-[350px] flex flex-col justify-center gap-6">
+                    <div className="flex flex-col gap-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">
+                            Acessar Painel
+                        </h1>
+                        <p className="text-sm text-muted-foreground">
+                            Acompanhe suas vendas pelo painel de parceiro
+                        </p>
+                    </div>
+
+                    <form className='space-y-4'>
+                        <div className='space-y-2'>
+                            <Label htmlFor="email">Seu Email</Label>
+                            <Input id="email" type="email" />
+                        </div>
+
+                        <Button className="w-full" type="submit">Acessar</Button>
+                    </form>
+
+                </div>
+            </div>
+
         </>
     )
 }
